01a24c69952bc25ce7898bcf4534c7587bd865a8
Istvan <dev@l1x.be>
2026-03-06 18:35:00 +0100

    Set sha256 for v0.1.0 release

--- Formula/qrst.rb
+++ Formula/qrst.rb
@@ -7,7 +7,7 @@ class Qrst < Formula
   depends_on arch: :arm64
 
   url "https://github.com/l1x/qrst/releases/download/v0.1.0/qrst-aarch64-apple-darwin.tar.gz"
-  sha256 "PLACEHOLDER"
+  sha256 "37f08b3a7b02ef5bdd67afcb3d7044b64806f13fa1e9d958405634c2fe0f505d"
 
   def install
     bin.install "qrst"
